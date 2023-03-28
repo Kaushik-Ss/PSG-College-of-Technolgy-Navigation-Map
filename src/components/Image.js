1ef20f52890284f45a17a3a4d57171d03e09d57b
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2023-03-29 02:28:19 +0530

    not working

--- src/components/Image.js
+++ src/components/Image.js
@@ -14,8 +14,31 @@ const ImageMap = () => {
   const imageRef = useRef(null);
 
   const handleMouseOver = (event) => {
-    const t=event.target;
-    setSelectedArea(event.target);
+
+
+    const t=document.getElementById("image-map");
+    console.log(t)
+    // t.style.filter = "blur(6px)";
+
+
+
+    
+console.log(event.target.getAttribute('data-unblur'))
+    if (event.target.getAttribute('data-unblur') === 'true') {
+    event.target.style.filter="none";
+    console.log(event.target);
+
+    const coords = event.target.getAttribute('coords').split(',');
+      const x1 = parseInt(coords[0]);
+      const y1 = parseInt(coords[1]);
+      const x2 = parseInt(coords[2]);
+      const y2 = parseInt(coords[3]);
+      const overlay = document.querySelector('#overlay');
+      overlay.innerHTML = `
+        <div class="area-highlight" style="top: ${y1}px; left: ${x1}px; width: ${x2 - x1}px; height: ${y2 - y1}px; filter: none;"></div>
+      `;
+    }
+
     // t.style.backgroundColor = 'yellow';
     // t.style.color = 'red';
     // // t.style.
@@ -39,6 +62,9 @@ const ImageMap = () => {
   const handleMouseOut = () => {
     setShowText(false);
     setSelectedArea(null);
+    const t=document.getElementById("image-map");
+    console.log(t)
+    // t.style.filter = "blur(0px)";
   };
 
   return (
@@ -47,9 +73,10 @@ const ImageMap = () => {
         src={k_block}
         alt="Your image"
         useMap="#image-map"
+        id="image-map"
         ref={imageRef}
       />
-
+<div id="overlay"></div>
       <map name="image-map">
         <area
           shape="rect"
@@ -57,16 +84,18 @@ const ImageMap = () => {
         data-text="DEPT OF. COMPUTER APPLICATTIONS"
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
-          className={selectedArea === 'area1' ? 'highlight' : ''}
+          className={selectedArea === 'AREA' ? 'highlight' : ''}
+          data-unblur="true" 
       
         />
         <area
           shape="rect"
           coords="490,637,692,460"
+          className={selectedArea === 'AREA' ? 'highlight' : ''}
           data-text="MCA COMPUTER LAB"
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
-          className={selectedArea === 'area1' ? 'highlight' : ''}
+        
         />
         
         {/* add more areas as needed */}
