de7bf3aa5799dc02c611357ed889a5698daf2935
Kaushik S <75175772+Kaushik-Ss@users.noreply.github.com>
2023-03-29 17:00:31 +0530

    working

---
+++ public/data_load.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+export default memo(function data_load() {
+  return (
+    <div>
+      
+    </div>
+  )
+})
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,16 @@
 import React, { useState, useRef, useEffect } from 'react';
+import k_block_data from '../static/data/k_block.json'; // assuming your JSON file is named computerLabMap.json
+
 import k_block from '../static/images/k_block.jpg';
 // import floating_data from './floating_data';
 import '../static/style/floating.css';
 import ImageMapper from './temp.js';
-
-
+import $ from "jquery";
+// import XLSX from 'xlsx';
+import * as XLSX from 'xlsx'
+// import 'data.csv' as data_file;
+import Papa from 'papaparse';
+import './data.csv';
 
 const ImageMap = () => {
   const [showText, setShowText] = useState(false);
@@ -12,111 +18,111 @@ const ImageMap = () => {
   const [textContent, setTextContent] = useState('');
   const [selectedArea, setSelectedArea] = useState(null);
   const imageRef = useRef(null);
+  const [areas, setAreas] = useState([]);
 
-  const handleMouseOver = (event) => {
+  useEffect(() => {
 
+    const handleMouseOver = (event) => {
+      const x = event.clientX;
+      const y = event.clientY;
 
-    const t=document.getElementById("image-map");
-    console.log(t)
-    // t.style.filter = "blur(6px)";
+      const text = event.currentTarget.dataset.text;
+      if (text) {
+        setTextPosition({ x, y });
+        setTextContent(text.split("<br/>").join("\n"));
+        setShowText(true);
+      } else {
+        setShowText(false);
+      }
+    };
 
-
-
-    
-console.log(event.target.getAttribute('data-unblur'))
-    if (event.target.getAttribute('data-unblur') === 'true') {
-    event.target.style.filter="none";
-    console.log(event.target);
-
-    const coords = event.target.getAttribute('coords').split(',');
-      const x1 = parseInt(coords[0]);
-      const y1 = parseInt(coords[1]);
-      const x2 = parseInt(coords[2]);
-      const y2 = parseInt(coords[3]);
-      const overlay = document.querySelector('#overlay');
-      overlay.innerHTML = `
-        <div class="area-highlight" style="top: ${y1}px; left: ${x1}px; width: ${x2 - x1}px; height: ${y2 - y1}px; filter: none;"></div>
-      `;
-    }
-
-    // t.style.backgroundColor = 'yellow';
-    // t.style.color = 'red';
-    // // t.style.
-    // t.classList.add('highlight');
-    console.log(t);
-    // t.style.background="red";
-    const x = event.clientX;
-    const y = event.clientY;
-
-    const text = event.currentTarget.dataset.text;
-    if (text) {
-      setTextPosition({ x, y });
-
-      setTextContent(text);
-      setShowText(true);
-    } else {
+    const handleMouseOut = () => {
       setShowText(false);
-    }
-  };
+      setSelectedArea(null);
+    };
 
-  const handleMouseOut = () => {
-    setShowText(false);
-    setSelectedArea(null);
-    const t=document.getElementById("image-map");
-    console.log(t)
-    // t.style.filter = "blur(0px)";
-  };
+    // Use jQuery inside the function
+    $('area').on('mouseover', handleMouseOver);
+    $('area').on('mouseout', handleMouseOut);
+
+    // Clean up the event listeners when the component unmounts
+    return () => {
+      $('area').off('mouseover', handleMouseOver);
+      $('area').off('mouseout', handleMouseOut);
+
+      $('#image-map area').hover(
+        function () { 
+            var coords = $(this).attr('coords').split(','),
+                width = $('.image-map-container').width(),
+                height = $('.image-map-container').height();
+            $('.image-map-container .map-selector').addClass('hover').css({
+                'left': coords[0]+'px',
+                'top': coords[1] + 'px',
+                'right': width - coords[2],
+                'bottom': height - coords[3]
+            })
+        },
+        function () { 
+            $('.image-map-container .map-selector').removeClass('hover').attr('style','');
+        }
+    )
+
+
+      
+    };
+  }, []);
+
 
   return (
+    <>
     <div style={{ position: 'relative' }}>
-      <img
-        src={k_block}
-        alt="Your image"
-        useMap="#image-map"
-        id="image-map"
-        ref={imageRef}
-      />
-<div id="overlay"></div>
-      <map name="image-map">
-        <area
-          shape="rect"
-          coords="340,831,414,929"
-        data-text="DEPT OF. COMPUTER APPLICATTIONS"
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-          className={selectedArea === 'AREA' ? 'highlight' : ''}
-          data-unblur="true" 
       
-        />
-        <area
-          shape="rect"
-          coords="490,637,692,460"
-          className={selectedArea === 'AREA' ? 'highlight' : ''}
-          data-text="MCA COMPUTER LAB"
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        
-        />
-        
-        {/* add more areas as needed */}
-      </map>
 
+<div className="image-map-container">
+    <img src={k_block} useMap="#image-map" ref={imageRef}/>
+    <div className="map-selector"></div>
+</div>
+
+
+<div id="overlay"></div>
+      <map name="image-map" id="image-map">
+{k_block_data.areas.map((area, index) => (
+          <area
+            key={index}
+            shape={area.shape}
+            coords={area.coords}
+            data-text={area.text}
+          />
+        ))}
+      </map>
+      
       {showText && (
-        <div
+        <div className='floating-text'
           style={{
+            display: 'block',
+            height: 'auto',
             position: 'fixed',
             top: textPosition.y,
             left: textPosition.x,
-            backgroundColor: 'white',
+            backgroundColor: 'blue',
+            whiteSpace: "pre-line",
+            borderRadius: '5px',
             boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)',
             padding: '10px',
             zIndex: 1,
-          }}
+            opacity: 1,
+            transition: 'opacity 200ms, display 200ms',
+            color: 'white'
+            
+          }
+        
+        }
         >
           {textContent}
         </div>
       )}
     </div>
+    </>
   );
 };
 
